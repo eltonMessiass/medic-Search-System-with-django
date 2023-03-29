74e5a0752e72d93cf99d7324da8ddca42c80dcda
eltonMessiass <eltonmessias10@gmail.com>
2023-03-29 23:57:37 +0200

    customizACAO DO ADMIN

--- medicSearch/admin.py
+++ medicSearch/admin.py
@@ -2,7 +2,15 @@ from django.contrib import admin
 from .models import *
 # Register your models here.
 
-admin.site.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'role', 'birthday')
+    list_display_links = ('user','role')
+    empty_value_display = 'vazio'
+    #filtro de hierarquia com datas
+    #date_hierarchy = 'created_at'
+    
+
+admin.site.register(Profile, ProfileAdmin)
 admin.site.register(State)
 admin.site.register(City)
 admin.site.register(Neighborhood)

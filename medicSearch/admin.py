022c2835fd1e3a63581d7ff64803c4486e7146a8
eltonMessiass <eltonmessias10@gmail.com>
2023-03-30 20:01:45 +0200

    comecando a customizacao  do medicSearch

--- medicSearch/admin.py
+++ medicSearch/admin.py
@@ -2,20 +2,11 @@ from django.contrib import admin
 from .models import *
 # Register your models here.
 
-class ProfileAdmin(admin.ModelAdmin):
-    list_display = ('user', 'role', 'birthday')
-    list_display_links = ('user','role')
-    empty_value_display = 'vazio'
-    list_filter = ('user__is_active',)
-    readonly_field = ('user')
-    search_fields = ('user__username',)
 
-    fields = ('user', ('role', ), 'image', 'birthday', 'specialities', 'addresses', )
-    #filtro de hierarquia com datas
-    #date_hierarchy = 'created_at'
-    
+   
+   
 
-admin.site.register(Profile, ProfileAdmin)
+admin.site.register(Profile)
 admin.site.register(State)
 admin.site.register(City)
 admin.site.register(Neighborhood)

9aadb15f101dd55c1400a02737ea71b51e15f8f2
eltonMessiass <eltonmessias10@gmail.com>
2023-03-31 23:46:52 +0200

    a

---
+++ medicSearch/urls/ProfileUrls.py
@@ -0,0 +1,2 @@
+from django.urls import path
+from medicSearch.views.ProfileView import list_profile_view
--- medicSearchAdmin/urls.py
+++ medicSearchAdmin/urls.py
@@ -19,6 +19,9 @@ from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
 
+from django.conf.urls import include
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',include('medicSearch.urls.HomeUrls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

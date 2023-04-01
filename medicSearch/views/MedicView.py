48247a3c6cc661477a40a09e901c9be9af0083c3
eltonMessiass <eltonmessias10@gmail.com>
2023-04-01 02:31:44 +0200

    home.html

--- medicSearch/views/MedicView.py
+++ medicSearch/views/MedicView.py
@@ -2,6 +2,8 @@ from django.http import HttpResponse
 
 from medicSearch.models import Profile
 
+from django.db.models import Q 
+
 
 def list_medics_view(request):
     name = request.GET.get("name")
@@ -13,7 +15,7 @@ def list_medics_view(request):
     medics = Profile.objects.filter(role=2)
 
     if name is not None and name != '':
-        medics = medics.filter(user__first_name__contains=name)
+        medics = medics.filter(Q(user__first_name__contains=name)| Q(user__username__contains=name))
     if speciality is not None:
         medics = medics.filter(speciality__id=speciality)
 
